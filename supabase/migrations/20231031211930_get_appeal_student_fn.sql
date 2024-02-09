9480e30af93178a65a16a4b762fad74f598c7d43
Ael <bluebayband@gmail.com>
2024-02-09 08:45:16 -0500

    update student appeal query to have is_read

--- supabase/migrations/20231031211930_get_appeal_student_fn.sql
+++ supabase/migrations/20231031211930_get_appeal_student_fn.sql
@@ -14,7 +14,8 @@ OR replace FUNCTION PUBLIC .get_student_appeals(sid uuid) RETURNS TABLE(
     assignment_id bigint,
     assignment_name text,
     appeal_id bigint,
-    created_at TIMESTAMP WITH TIME ZONE
+    created_at TIMESTAMP WITH TIME ZONE,
+    is_read BOOLEAN
 ) AS $$ BEGIN
     RETURN query
     SELECT
@@ -30,7 +31,8 @@ OR replace FUNCTION PUBLIC .get_student_appeals(sid uuid) RETURNS TABLE(
         A .id AS assignment_id,
         A .assignment_name AS assignment_name,
         app.id AS appeal_id,
-        app.created_at AS created_at
+        app.created_at AS created_at,
+        msg.is_read AS is_read
     FROM
         "Students" AS s
         INNER JOIN "StudentAppeal" AS sa ON s.id = sa.student_id
@@ -39,6 +41,23 @@ OR replace FUNCTION PUBLIC .get_student_appeals(sid uuid) RETURNS TABLE(
         INNER JOIN "Courses" AS C ON A .course_id = C .id
         INNER JOIN "ProfessorCourse" AS pc ON C .id = pc.course_id
         INNER JOIN "Professors" AS p ON pc.professor_id = p.id
+        LEFT JOIN (
+            SELECT
+                m.appeal_id,
+                m.is_read
+            FROM
+                PUBLIC."Messages" AS m
+                INNER JOIN (
+                    SELECT
+                        m.appeal_id,
+                        MAX(m.created_at) AS max_created_at
+                    FROM
+                        PUBLIC."Messages" AS m
+                    GROUP BY
+                        m.appeal_id
+                ) AS latest_msg ON m.appeal_id = latest_msg.appeal_id
+                AND m.created_at = latest_msg.max_created_at
+        ) AS msg ON app.id = msg.appeal_id
     WHERE
         s.id = sid
     ORDER BY
